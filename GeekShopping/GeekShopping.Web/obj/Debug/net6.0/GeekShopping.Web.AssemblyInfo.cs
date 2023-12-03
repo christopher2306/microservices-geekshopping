84ce7928b5dc55eb816d9e8c5107a0f987ce321d
Christopher Antony <christophercarreira2306@gmail.com>
2023-12-03 15:02:02 -0300

    Adding the class Startup.cs

--- GeekShopping/GeekShopping.Web/obj/Debug/net6.0/GeekShopping.Web.AssemblyInfo.cs
+++ GeekShopping/GeekShopping.Web/obj/Debug/net6.0/GeekShopping.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae69877f3cd04e8ac46d38f9e5c692334f6e3a09")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
